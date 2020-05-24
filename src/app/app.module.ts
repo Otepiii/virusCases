522100025bc892d0a1a7ce9b4bdad635c572f1da
joseph Evangelista <oootep123@gmail.com>
2020-05-24 10:44:29 -0400

    added imports and variable to cases-stat component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,6 +26,7 @@ import { MatSliderModule } from '@angular/material/slider';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatSelectModule } from '@angular/material/select';
+import { ChartsModule } from 'ng2-charts';
 
 @NgModule({
   declarations: [
@@ -56,6 +57,7 @@ import { MatSelectModule } from '@angular/material/select';
     MatSlideToggleModule,
     MatButtonToggleModule,
     MatSelectModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/cases-stat/cases-stat.component.ts
+++ src/app/cases-stat/cases-stat.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartOptions, ChartType, ChartDataSets } from 'chart.js';
+import { Label } from 'ng2-charts';
+import { ApiService } from '../api.service';
+import { Statistic } from '../statistic';
 
 @Component({
   selector: 'app-cases-stat',
@@ -6,8 +10,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cases-stat.component.scss']
 })
 export class CasesStatComponent implements OnInit {
+  stats:Statistic[] = [];
+  label = 'Positive';
+  isLoadingResults = true;
+  barChartOptions = {
+    responsive: true,
+  };
+  barChartLabels: label[] = [];
+  barChartType: ChartType = 'bar';
+  barChartLegend = true;
+  barChartPlugins = [];
+  barChartData: ChartDataSets[] = [{data: [], backgroundColor: [], label: this.label}];
 
-  constructor() { }
+  constructor(private api: ApiService) { }
 
   ngOnInit(): void {
   }
