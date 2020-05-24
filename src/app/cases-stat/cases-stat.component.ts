f2012367ae8a872b83b43301c9313eba86e0583e
joseph Evangelista <oootep123@gmail.com>
2020-05-24 11:02:09 -0400

    added function to load statistic data from REST API then implement it as a bar chart

--- src/app/cases-stat/cases-stat.component.ts
+++ src/app/cases-stat/cases-stat.component.ts
@@ -16,11 +16,37 @@ export class CasesStatComponent implements OnInit {
   barChartOptions = {
     responsive: true,
   };
-  barChartLabels: label[] = [];
+  barChartLabels: Label[] = [];
   barChartType: ChartType = 'bar';
   barChartLegend = true;
   barChartPlugins = [];
   barChartData: ChartDataSets[] = [{data: [], backgroundColor: [], label: this.label}];
+  getStatistic(status: string) {
+    this.barChartData = [{data: [], backgroundColor: [], label: this.label}];
+    this.barChartLabels = [];
+    this.api.getStatistic(status)
+    .subscribe((res: any) =>{
+      this.stats = res;
+      const chartdata: number[] = [];
+      const chartcolor: string[] = [];
+      this.stats.forEach((stat) => {
+        this.barChartLabels.push(stat._id.date);
+        chartdata.push(stat.count);
+        if (this.label === 'Positive') {
+          chartcolor.push('rgba(255,165,0,0.5)');
+        } else if (this.label === 'Dead') {
+          chartcolor.push('rgba(255,0,0,0.5)');
+        } else {
+          chartcolor.push('rgba(0,255,0,0.5)');
+        }
+      });
+      this.barChartData = [{data: chartdata, backgroundColor: chartcolor, label: this.label}];
+      this.isLoadingResults = false;
+    }, err => {
+      console.log(err);
+      this.isLoadingResults = false;
+    });
+  }
 
   constructor(private api: ApiService) { }
 
